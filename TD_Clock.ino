801b8248b5ccd2ffa37970261429c6457122f9fa
lcy-mich <michael.luk.25@ucl.ac.uk>
2025-10-13 10:25:08 +0100

    added source

---
+++ TD_Clock.ino
@@ -0,0 +1,110 @@
+#include "TFT_eSPI.h"
+#include "SPI.h"
+
+#define L_BUTTON 0
+#define R_BUTTON 35
+#define HEIGHT 135
+#define WIDTH 240
+
+TFT_eSPI tft = TFT_eSPI();
+
+#include "FastTrig.h"
+
+#include "time.h"
+#define NTP "pool.ntp.org"
+#define GMT_OFFSET 0
+#define DAYLIGHT_OFFSET 3600
+
+struct tm t_struct;
+char t_str[9];
+bool isAnalog = false;
+bool clearedScreen = false;
+
+#define DEBOUNCE_TIME 200
+unsigned long debounceTime;
+
+#include "WiFi.h"
+#include "esp_eap_client.h"
+#include "secrets.h"
+
+void setupWifi() {
+  const char* USER = USERNAME;
+  const char* PASS = PASSWORD;
+  
+  WiFi.mode(WIFI_STA);
+
+  esp_eap_client_set_identity((uint8_t *)USER, strlen(USER));
+  esp_eap_client_set_username((uint8_t *)USER, strlen(USER));
+  esp_eap_client_set_password((uint8_t *)PASS, strlen(PASS));
+
+  esp_wifi_sta_enterprise_enable();
+  WiFi.begin("eduroam");
+  
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    delay(500);
+  }
+}
+
+void ButtonPressed(){
+  if ((millis() - debounceTime) < DEBOUNCE_TIME) { return; }
+  debounceTime = millis();
+
+  isAnalog = !isAnalog;
+  clearedScreen = false;
+}
+
+void lButtonPressed() {
+  ButtonPressed();
+}
+void rButtonPressed() {
+  ButtonPressed();
+}
+
+void render() {
+  strftime(t_str, 9, "%T", &t_struct);
+
+  if (!clearedScreen) {
+    tft.fillScreen(TFT_BLACK);
+    clearedScreen = true;
+    if (isAnalog) { tft.drawSmoothCircle(WIDTH*.5, HEIGHT*.5, HEIGHT*.5, TFT_WHITE, TFT_WHITE); }
+  }
+  getLocalTime(&t_struct);
+  if (isAnalog) {
+    
+    uint8_t h_centre = HEIGHT*.5;
+    uint8_t w_centre = WIDTH*.5;
+
+    uint8_t radius = h_centre;
+
+    tft.fillSmoothCircle(w_centre, h_centre, radius, TFT_BLACK);
+
+  } 
+  else {
+    tft.drawString(t_str, WIDTH/2, HEIGHT/2);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  setupWifi();
+  configTime(GMT_OFFSET, DAYLIGHT_OFFSET, NTP);
+  
+  tft.init();
+  tft.setRotation(1);
+
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextFont(7);
+  tft.setTextSize(1);
+
+  debounceTime = millis();
+  attachInterrupt(digitalPinToInterrupt(L_BUTTON), lButtonPressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(R_BUTTON), rButtonPressed, RISING);
+}
+
+void loop() {
+  delay(100);
+  render();
+  //Serial.println(t_str);
+}
